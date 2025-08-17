9163645ab272aca4f589bf87a787e001c20d03cd
Phisherman <infernospyder@gmail.com>
2025-08-17 23:47:34 +0100

    Remove unused vite.ts file

--- dist/vite.d.ts
+++
@@ -1,4 +0,0 @@
-import { type Express } from "express";
-export declare function log(message: string, source?: string): void;
-export declare function serveStatic(app: Express): void;
-//# sourceMappingURL=vite.d.ts.map
